a7c557722ddf3dc73438a02960210e2b3542bb06
PRASHANNA RAI <prai931024@gmail.com>
2025-04-09 16:39:50 +0200

    updated source code

--- src/component/UnServiceDevice.jsx
+++ src/component/UnServiceDevice.jsx
@@ -3,6 +3,8 @@ import { useQuery, gql } from "@apollo/client";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { ProgressSpinner } from "primereact/progressspinner"; // PrimeReact spinner
+import { Panel } from 'primereact/panel';
+import { Splitter, SplitterPanel } from 'primereact/splitter';
 
 // GraphQL query
 const GET_UNSERVICED_DEVICES_COMPONENTS = gql`
@@ -41,16 +43,33 @@ export default function UnServiceDevice() {
   if (error) {
     return <p>Error fetching data: {error.message}</p>;
   }
-  
+
   if (!loading) {
     return (
-        <div className="card">
-          <h2>Unserviced Devices</h2>
-          <DataTable value={result} showGridlines tableStyle={{ minWidth: "50rem" }}>
-            <Column field="device_serial_number" header="Device ID" />
-            <Column field="component_serial_number" header="Component ID" />
-            <Column field="subcomponent_serial_number" header="Sub Component ID" />
-          </DataTable>
+        <div>
+            <Splitter style={{ height: '50px'}} className="mb-2 mt-4">
+                <SplitterPanel className="flex align-items-center justify-content-center" >
+                    <Panel header="Total Farms" style={{ height: '100%', width: '100%' }}>
+                        <p className="m-0">1</p>
+                    </Panel>
+                </SplitterPanel>
+                <SplitterPanel className="flex align-items-center justify-content-center" >
+                    <Panel header="Total Devices" style={{ height: '100%', width: '100%' }}>
+                        <p className="m-0">10</p>
+                    </Panel>
+                </SplitterPanel>
+            </Splitter>
+            
+            <br />
+
+            <div className="card">
+                <h2>Unserviced Devices</h2>
+                <DataTable value={result} showGridlines tableStyle={{ minWidth: "50rem" }}>
+                    <Column field="device_serial_number" header="Device ID" />
+                    <Column field="component_serial_number" header="Component ID" />
+                    <Column field="subcomponent_serial_number" header="Sub Component ID" />
+                </DataTable>
+            </div>
         </div>
       );
   }
